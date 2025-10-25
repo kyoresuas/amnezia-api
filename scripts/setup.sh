ce2d79b2b3c993263d8e101fd155e68f99b692e2
kyoresuas <143986688+kyoresuas@users.noreply.github.com>
2025-10-25 12:28:26 +0800

    fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -179,7 +179,7 @@ server {
     server_name _;
 
     location / {
-        proxy_pass http://127.0.0.1:4000;
+        proxy_pass http://127.0.0.1:4001;
         proxy_http_version 1.1;
         proxy_set_header Host $host;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
